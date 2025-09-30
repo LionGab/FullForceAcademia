a927ac43b8d949c42168368453d52dd83beed8e6
LionAlpha <gabrielvesz_@hotmail.com>
2025-09-29 23:28:35 -0400

    Update vite.config.js

--- fullforce-site/vite.config.js
+++ fullforce-site/vite.config.js
@@ -4,31 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Base path para GitHub Pages
-  // Se o repo for github.com/usuario/fullforce-site, use '/fullforce-site/'
-  // Se for github.com/usuario/usuario.github.io, use '/'
-  base: process.env.GITHUB_ACTIONS 
-    ? (process.env.GITHUB_REPOSITORY?.split('/')[1] || '/fullforce-site/').replace(/^/, '/').replace(/\/$/, '') + '/'
-    : '/',
-  server: {
-    port: 3000,
-    open: true,
-  },
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    emptyOutDir: true,
-    sourcemap: false,
-    minify: 'esbuild',
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom'],
-        },
-      },
-    },
-  },
-  optimizeDeps: {
-    include: ['react', 'react-dom'],
-  },
+  base: '/FullForceAcademia/', // Nome do repositório
 })
