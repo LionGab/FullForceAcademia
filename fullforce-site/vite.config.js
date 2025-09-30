1b534abd770272b2b02b3fdbdb86bca43274b276
LionAlpha <gabrielvesz_@hotmail.com>
2025-09-30 08:47:31 -0400

    Change base path for Netlify deployment
    Updated base path for deployment to Netlify.
    
    Signed-off-by: LionAlpha <gabrielvesz_@hotmail.com>

--- fullforce-site/vite.config.js
+++ fullforce-site/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/FullForceAcademia/', // Nome do repositório
+  base: '/', // Corrigir para barra simples para deploy Netlify
 })
